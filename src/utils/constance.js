bfc2dd2156e893f582b8374e129ffeaad7754f29
madhavpatel236 <madhavpatel236@gmail.com>
2024-08-02 21:28:09 +0530

    error solved

--- src/utils/constance.js
+++ src/utils/constance.js
@@ -2,7 +2,7 @@ export const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
   export const  SWIGGY_HOME_API =
-  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.4825588&lng=70.0589194&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.4825588&lng=70.0589194&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
 
   export const SWIGGY_REST_DETAILS_API =
-  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.4825588&lng=70.0589194&restaurantId="
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.4825588&lng=70.0589194&restaurantId="
