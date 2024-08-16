9d3b331287be1d269a662be15b44c3f84f893371
madhavpatel236 <madhavpatel236@gmail.com>
2024-08-16 16:53:00 +0530

    API changes

--- src/utils/constance.js
+++ src/utils/constance.js
@@ -2,7 +2,9 @@ export const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
   export const  SWIGGY_HOME_API =
-  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.4825588&lng=70.0589194&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+  // "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.4825588&lng=70.0589194&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+  "https://food-ordering-app-server.vercel.app/api/proxy/swiggy/dapi/restaurants/list/v5?lat=18.5204303&lng=73.8567437&&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+
 
   export const SWIGGY_REST_DETAILS_API =
   "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.4833391&lng=70.05802659999999&restaurantId="
