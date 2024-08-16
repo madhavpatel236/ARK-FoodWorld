0a1635e492ebe344d40fb2538109ac64cf545ce5
madhavpatel236 <madhavpatel236@gmail.com>
2024-08-16 17:50:50 +0530

    API Change

--- src/utils/constance.js
+++ src/utils/constance.js
@@ -6,8 +6,8 @@ export const SWIGGY_HOME_API =
   "https://food-ordering-app-server.vercel.app/api/proxy/swiggy/dapi/restaurants/list/v5?lat=18.5204303&lng=73.8567437&&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
 
 export const SWIGGY_REST_DETAILS_API =
-  'https://food-ordering-app-server.vercel.app/api/proxy/swiggy/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.5204303&lng=73.8567437&restaurantId='
-// "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.4833391&lng=70.05802659999999&restaurantId="
+'https://food-ordering-app-server.vercel.app/api/proxy/swiggy/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.5204303&lng=73.8567437&restaurantId='
+  // "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.4833391&lng=70.05802659999999&restaurantId="
 
 // export const SWIGGY_SEARCH_API = // NEED to understand the variable concept in the API
 // `https://www.swiggy.com/dapi/restaurants/search/suggest?lat=22.30080&lng=73.20430&str=${search}&trackingId=undefined&includeIMItem=true`
