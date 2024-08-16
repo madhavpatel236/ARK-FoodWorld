23aca71c41dd9e58b93776fd8dd6bddff5dcfdd2
madhavpatel236 <madhavpatel236@gmail.com>
2024-08-16 17:47:25 +0530

    CORS

--- src/utils/constance.js
+++ src/utils/constance.js
@@ -3,10 +3,10 @@ export const CDN_URL =
 
 export const SWIGGY_HOME_API =
   // "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.4825588&lng=70.0589194&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-  "https://food-ordering-app-server.vercel.app/api/proxy/swiggy/dapi/restaurants/list/v5?lat=22.4825588&lng=70.0589194&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+  "https://food-ordering-app-server.vercel.app/api/proxy/swiggy/dapi/restaurants/list/v5?lat=18.5204303&lng=73.8567437&&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
 
 export const SWIGGY_REST_DETAILS_API =
-  'https://food-ordering-app-server.vercel.app/api/proxy/swiggy/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.4833391&lng=70.05802659999999&restaurantId='
+  'https://food-ordering-app-server.vercel.app/api/proxy/swiggy/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.5204303&lng=73.8567437&restaurantId='
 // "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.4833391&lng=70.05802659999999&restaurantId="
 
 // export const SWIGGY_SEARCH_API = // NEED to understand the variable concept in the API
